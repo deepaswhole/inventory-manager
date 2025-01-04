45fceb9d385f5bcc41d7d64d4acbd4824d696f7e
deepaswhole <lanwong0419@gmail.com>
2025-01-04 08:14:21 +0800

    Added add product feature

--- inventory_manager.py
+++ inventory_manager.py
@@ -1,4 +1,8 @@
 print("welcome to inventory manager")
 inventory = []
 
+def add_product(name, quantity, price):
+    inventory.append({"name": name, "quantity": quantity, "price":price})
+    print(f"Inventory : {name} is added.")
+
 
