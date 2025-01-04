2d514df285e7589834d91fe5ffcc4bf008990202
deepaswhole <lanwong0419@gmail.com>
2025-01-04 08:49:04 +0800

    added view product feature

--- inventory_manager.py
+++ inventory_manager.py
@@ -5,4 +5,11 @@ def add_product(name, quantity, price):
     inventory.append({"name": name, "quantity": quantity, "price":price})
     print(f"Inventory : {name} is added.")
 
+def view_inventory():
+    if not inventory:
+        print("empty inventory")
+    else:
+        for i, product in enumerate(inventory,1):
+            print(f"{i}, Name :{product['name']}, Quantity: {product['quantity']}, Price : {product['price']}")
+
 
