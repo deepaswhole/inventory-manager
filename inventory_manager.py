786b6f0eeacd225122ae24350f57a8d0daa822fe
deepaswhole <lanwong0419@gmail.com>
2025-01-05 19:22:28 +0800

    conflict handling branch3

--- inventory_manager.py
+++ inventory_manager.py
@@ -57,7 +57,7 @@ def load_inventory():
         with open("inventory.json", 'r') as file:
             inventory = json.load(file)
         print("inventory loaded from file!")
-        print('branch2')
+        print('branch3')
     except FileNotFoundError:
         print("No saved inventory found!")
 
